60efe4fc5b5635e7196a525ff0c0d11434903c9d
Caroline Nunes <nunescaroline905@gmail.com>
2023-09-24 14:28:38 -0300

    fix: update products table collun id

--- src/database/seeders/20230923184001-Products.js
+++ src/database/seeders/20230923184001-Products.js
@@ -4,30 +4,37 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('products', [
       {
-        product_name: 'CAIXA DE COPO 200ML',
+        id: 1001,
+        product_name: 'CAIXA DE COPO 200 ML',
         description: '48 UNIDADES',
       },
       {
+        id: 1002,
         product_name: 'AGUA 510 ML SEM GÁS',
         description: '12 UNIDADES',
       },
       {
+        id: 1003,
         product_name: 'AGUA 510 ML COM GÁS',
         description: '12 UNIDADES',
       },
       {
+        id: 1004,
         product_name: 'AGUA 1500 ML SEM GÁS',
         description: '12 UNIDADES',
       },
       {
+        id: 1005,
         product_name: 'AGUA 1500 ML COM GÁS',
         description: '12 UNIDADES',
       },
       {
+        id: 1006,
         product_name: 'AGUA 5 LITROS',
         description: '4 UNIDADES',
       },
       {
+        id: 1007,
         product_name: 'AGUA 20 LITROS',
         description: '1 UNIDADE',
       },
