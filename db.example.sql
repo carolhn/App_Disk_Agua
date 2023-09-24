772a83200d2bdebe2b1d3cb2a0b4d9c4201559f5
Caroline Nunes <nunescaroline905@gmail.com>
2023-09-23 23:08:13 -0300

    fix: adjusting the db reference file

--- db.example.sql
+++ db.example.sql
@@ -7,8 +7,7 @@ USE `Disk-app`;
 CREATE TABLE
     IF NOT EXISTS users (
         id INT NOT NULL AUTO_INCREMENT,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL,
+        usuario VARCHAR(100) NOT NULL,
         password VARCHAR(32) NOT NULL,
         role VARCHAR(20) NOT NULL,
     );
@@ -19,20 +18,19 @@ CREATE TABLE
         name VARCHAR(100) NOT NULL,
         phone VARCHAR(20) NOT NULL,
         type VARCHAR(20) NOT NULL,
-`CPF / CNPJ`VARCHAR(20) NOT NULL,
+        `CPF / CNPJ` VARCHAR(20) NOT NULL,
         address VARCHAR(100) NOT NULL,
         PRIMARY KEY (id),
-        UNIQUE KEY`CPF / CNPJ` (`CPF / CNPJ`)
+        UNIQUE KEY `CPF / CNPJ` (`CPF / CNPJ`)
     );
 
 CREATE TABLE
     IF NOT EXISTS products (
         id INT NOT NULL AUTO_INCREMENT,
-        name VARCHAR(100) NOT NULL,
-        price DECIMAL(4, 2) NOT NULL,
-        url_image VARCHAR(200) NOT NULL DEFAULT '',
+        product_name VARCHAR(100) NOT NULL,
+        description VARCHAR(100) NOT NULL,
         PRIMARY KEY(id),
-        UNIQUE KEY`name` (name)
+        UNIQUE KEY `name` (name)
     );
 
 CREATE TABLE
