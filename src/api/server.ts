ce3dded65cef20578d9e5ee7c01106cd929c365d
Caroline Nunes <nunescaroline905@gmail.com>
2023-09-29 17:11:37 -0300

    fix: update server.ts file

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import routes from './routes/index';
+import router from './routes/index';
 import { handleErrors } from './middlewares/handleErrors';
 import '@utils/typeorm';
 
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(routes);
+app.use(router);
 app.use(handleErrors);
 
 app.listen(3333, () => {
