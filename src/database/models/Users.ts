83997d67ea38c07e4b0079851b7b433397bafcb2
Caroline Nunes <nunescaroline905@gmail.com>
2023-10-03 17:12:41 -0300

    fix: update model users

--- src/database/models/Users.ts
+++ src/database/models/Users.ts
@@ -1,43 +1,46 @@
-import { DataTypes, Model } from "sequelize";
+import { Model, DataTypes } from 'sequelize';
 import db from './index';
 
 class Users extends Model {
-  declare id: number;
-  declare name: string;
-  declare email: string;
-  declare password: string;
-  declare role: string;
+  id!: number;
+  name!: string;
+  email!: string;
+  password!: string;
+  role!: 'administrator' | 'employee';
 }
 
-Users.init({
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
+Users.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-}, {
-  underscored: true,
-  sequelize: db,
-  tableName: 'users',
-  timestamps: false,
-});
+  {
+    sequelize: db,
+    tableName: 'users',
+    timestamps: false,
+    modelName: 'Users',
+  }
+);
 
 export default Users;
