810efa25ab561d9c02b58a566e0f34b7ca72af6f
Caroline Nunes <nunescaroline905@gmail.com>
2023-10-03 17:14:17 -0300

    fix: improving the code, making it object-oriented

--- src/api/services/AdminService.ts
+++ src/api/services/AdminService.ts
@@ -1,40 +1,42 @@
-import { IAdmin, NewAdmin } from '../interfaces/IAdmin';
+import { IAdmin, IAdminService, NewAdmin } from '../interfaces/IAdmin';
 import Users from '../../database/models/Users';
 import { createHash } from '../../utils/authentication/crypto';
 
-const getUserByEmail = async (email: string): Promise<IAdmin | null> => {
-  const user = await Users.findOne({
-    where: { email },
-    attributes: { exclude: ['password'] },
-  });
-  return user ? user.get() : null;
-};
+export default class AdminService implements IAdminService {
 
-const registerNewUser = async (newUser: NewAdmin): Promise<{ type: number; message: string }> => {
-  try {
-    const existingUser = await getUserByEmail(newUser.email);
-
-    if (existingUser) {
-      return { type: 409, message: 'Usuário já existe no banco de dados' };
-    }
-
-    const passwordHash = createHash(newUser.password);
-
-    const createNewUser = await Users.create({
-      name: newUser.name,
-      email: newUser.email,
-      password: passwordHash,
-      role: newUser.role,
+  async getUserByEmail(email: string): Promise<IAdmin | null> {
+    const user = await Users.findOne({
+      where: { email },
+      attributes: { exclude: ['password'] },
     });
+    return user ? user.get() : null;
+  };
 
-    if (createNewUser) {
-      return { type: 201, message: 'Usuário criado' };
-    } else {
-      throw new Error('Erro ao criar usuário');
+
+  async registerNewUser(newUser: NewAdmin): Promise<{ type: number; message: string }> {
+    try {
+      const existingUser = await this.getUserByEmail(newUser.email);
+
+      if (existingUser) {
+        return { type: 409, message: 'Usuário já existe no banco de dados' };
+      }
+
+      const passwordHash = createHash(newUser.password);
+
+      const createNewUser = await Users.create({
+        name: newUser.name,
+        email: newUser.email,
+        password: passwordHash,
+        role: newUser.role,
+      });
+
+      if (createNewUser) {
+        return { type: 201, message: 'Usuário criado' };
+      } else {
+        throw new Error('Erro ao criar usuário');
+      }
+    } catch (error) {
+      return { type: 500, message: 'Erro interno do servidor' };
     }
-  } catch (error) {
-    return { type: 500, message: 'Erro interno do servidor' };
   }
-};
-
-export { getUserByEmail, registerNewUser };
+}
