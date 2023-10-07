f735adbf076846271cad3d585166a89ba4fb7c81
Caroline Nunes <nunescaroline905@gmail.com>
2023-10-07 12:59:51 -0300

    feat: creating interface to return a new response

--- src/api/interfaces/IAuth.ts
+++ src/api/interfaces/IAuth.ts
@@ -1,4 +1,15 @@
+import Users from '../../database/models/Users';
+
 export interface IAuth {
     email: string;
     password: string;
+}
+
+export interface IToken {
+    id: number;
+}
+
+export interface IResponse {
+    user: Users;
+    token: string;
 }
