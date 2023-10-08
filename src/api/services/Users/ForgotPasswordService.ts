9f5851e8201b397f4a62967a636d48575ad4fcb6
Caroline Nunes <nunescaroline905@gmail.com>
2023-10-08 16:25:04 -0300

    feat: creating function to reset password

--- src/api/services/Users/ForgotPasswordService.ts
+++ src/api/services/Users/ForgotPasswordService.ts
@@ -1,7 +1,9 @@
-import { IRequest } from "@api/interfaces/Users/IForgotPassword";
+import { IRequest, IRequestPassword } from "@api/interfaces/Users/IForgotPassword";
 import UserService from "./UserService";
 import UserTokenService from "./UserTokenService";
 import AppError from "@utils/errors/AppError";
+import { createHash } from "@utils/authentication/crypto";
+import { isTokenExpired } from "@utils/authentication/token";
 
 export default class ForgotPasswordEmailService {
     constructor(
@@ -19,5 +21,26 @@ export default class ForgotPasswordEmailService {
     const userToken = await this.userTokenService.generateToken(user.id);
     console.log('--------------TOKEN------------', userToken);
     
+  }
+
+  async resetPassowordService({ token, password }: IRequestPassword): Promise<void> {
+    const userToken = await this.userTokenService.findByToken(token);
+
+    if(!userToken) {
+        throw new AppError('Token não encontrado')
+    }
+
+    const user = await this.userService.findById(userToken.user_id);
+    if(!user) {
+        throw new AppError('Usuário não encontrado')
+    }
+
+    if (isTokenExpired(userToken.created_at, 2)) {
+        throw new AppError('Token expirado')
+    }
+
+    user.password = password;
+
+    const passwordHash = createHash(user.password);
   }
 }
