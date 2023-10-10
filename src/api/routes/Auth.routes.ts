49ba8338b3fa6deab4ca57e83648351c3ceea616
Caroline Nunes <nunescaroline905@gmail.com>
2023-10-10 01:15:06 -0300

    fix: updating Auth route

--- src/api/routes/Auth.routes.ts
+++ src/api/routes/Auth.routes.ts
@@ -11,7 +11,7 @@ class AuthRoutes {
   }
 
   private initializeRoutes(): void {
-    this.router.post('/',validationAuth, this.authController.authUser);
+    this.router.post('/', validationAuth, this.authController.authUser);
   }
 }
 
