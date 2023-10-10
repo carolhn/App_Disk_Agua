bc79018169b0656814fa021b2fe48b5d4d0272df
Caroline Nunes <nunescaroline905@gmail.com>
2023-10-10 01:05:00 -0300

    fix: adjusting the generateToken function of the userToken service

--- src/api/services/Users/UserTokenService.ts
+++ src/api/services/Users/UserTokenService.ts
@@ -1,25 +1,32 @@
 import UserToken from '@database/models/UserToken';
-import { IUserToken } from '../../interfaces/Users/IUserToken';
+import { v4 as uuidv4 } from 'uuid'; // Importe a biblioteca uuid e atribua um nome a ela (v4) para evitar conflitos de nome
 
-export default class UserTokenService implements IUserToken {
+
+export default class UserTokenService {
 
   async findByToken(token: string): Promise<UserToken | null> {
     const userToken = await UserToken.findOne({
       where: { token },
     });
     return userToken;
-  }
+  };
 
-  async generateToken(userId: number): Promise<UserToken> {
-    const userToken = await UserToken.create({
-      userId,
-    });
+  // async generateToken(user_id: string): Promise<UserToken> {
+  //   const userToken = await UserToken.create({
+  //     user_id: user_id,     
+  //   });
+    
+  //   return userToken;
+  // };
 
-    return userToken;
-  }
+  async generateToken(user_id: string): Promise<{ token: string, userToken: UserToken }> {
+  const token = uuidv4();
 
-  async save(userToken: UserToken): Promise<UserToken> {
-    await userToken.save();
-    return userToken;
-  }
-}
+  const userToken = await UserToken.create({
+    user_id: user_id,
+    token: token,
+  });
+
+  return { token, userToken };
+};
+};
